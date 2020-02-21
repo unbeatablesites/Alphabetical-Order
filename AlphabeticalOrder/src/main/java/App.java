25bd695786d33cbff8781613b06b57820d670b6c
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-21 18:21:28 -0500

    Adding Scanner

--- AlphabeticalOrder/src/main/java/App.java
+++ AlphabeticalOrder/src/main/java/App.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+
+    }
+
+    public static void askForLastName(){
+
+        System.out.println("What's your last name?");
+        Scanner scan = new Scanner(System.in);
+
+
+
+
 
     }
 
