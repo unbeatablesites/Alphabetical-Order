f75e05fd537e7834f12f9eb87d7ea179bd9ef004
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-21 18:30:19 -0500

    do logic

--- AlphabeticalOrder/src/main/java/App.java
+++ AlphabeticalOrder/src/main/java/App.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-
+        askForLastName();
     }
 
     public static void askForLastName(){
 
         System.out.println("What's your last name?");
         Scanner scan = new Scanner(System.in);
+
+        String userInputLastName = scan.nextLine().toUpperCase();
+
+
+        System.out.println(userInputLastName);
+
 
 
 
