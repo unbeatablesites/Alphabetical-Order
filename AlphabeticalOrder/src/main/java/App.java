9f0f0dc9346088ee3a4ac0b1215bd6f0b23eb565
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-21 18:47:54 -0500

    do logic

--- AlphabeticalOrder/src/main/java/App.java
+++ AlphabeticalOrder/src/main/java/App.java
@@ -16,10 +16,21 @@ public class App {
 
         int index = str1.compareTo(userInputLastName);
 
-        System.out.println(index);
 
 
+        if(index >= 25 && index <= 23){
+            System.out.println("You don't have to wait long "+userInputLastName+".");
+        }else if(index >= 23 && index <= 16){
+            System.out.println("That's not bad "+userInputLastName+".");
+        }else if(index >= 16 && index <= 7){
+            System.out.println("Looks like a bit of a wait "+userInputLastName+".");
+        }else if(index >= 7 && index <= 1){
+            System.out.println("It's gonna be a while "+userInputLastName+".");
+        }else{
 
+        }
+
+            System.out.println(index);
 
     }
 
